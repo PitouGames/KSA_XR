1f5790a7488d64c4decdce97e3f3d1e02a1ee231
PitouGames <pitou.games@gmail.com>
2026-03-04 20:55:58 +0100

    Fix ImGUI error when folding the debug window

--- Sources/KSA.XR/DebugUI.cs
+++ Sources/KSA.XR/DebugUI.cs
@@ -95,7 +95,7 @@ namespace KSA.XR
 			ImGuiMenuPatcher.ActionAddedAtMenuEnd.Add("View", () =>
 			{
 				ImGui.Separator();
-				ImGui.MenuItem("OpenXR Debug Window", "", ref WindowOpen);
+				ImGui.MenuItem("OpenXR Debug Window", "", ref windowOpen);
 			});
 		}
 
@@ -103,11 +103,15 @@ namespace KSA.XR
 		bool XrSessionStarted = false;
 		float renderBufferResoltuionScale = 1f;
 
-		public bool WindowOpen = true;
+		private bool windowOpen = true;
+
 		public void StatusWindow()
 		{
+			if (!windowOpen) {
+				return;
+			}
 
-			if (WindowOpen && ImGui.Begin("KSA_XR", ref WindowOpen))
+			if (ImGui.Begin("KSA_XR", ref windowOpen))
 			{
 				ImGui.TextDisabled("You can close this window and re-open it from the View menu");
 
@@ -180,8 +184,8 @@ namespace KSA.XR
 				{
 					ImGui.Text("OpenXR not initialized.");
 				}
-				ImGui.End();
 			}
+			ImGui.End();
 		}
 	}
 }
